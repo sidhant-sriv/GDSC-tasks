82d2cb4bad15d1986126a900ed549597ab318579
sidhant-sriv <sidhant.sriv@gmail.com>
2023-04-12 23:23:14 +0530

    figuring out basics of queries and mutations

--- backend-task-1/index.js
+++ backend-task-1/index.js
@@ -1,23 +1,39 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+const { ApolloServer } = require("apollo-server");
+const { readFileSync } = require("fs");
+
+const typeDefs = readFileSync("./schema.graphql", "utf8");
+
+let posts = [
+  {
+    id: "post-0",
+    title: "Hello, World",
+    body: "Kendrick Lamar is really good",
+  },
+];
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of some random social media site idrk`
-  }
-}
+    info: () => `This is the API of some random social media site idrk`,
+    feed: () => posts,
+  },
+  Mutation: {
+    create: (parent, args) => {
+      //Right now, just adding shit to the posts array on line 6(?)
+      let c = posts.length;
+      const post = {
+        id: `post-${c++}`,
+        title: args.title,
+        body: args.body,
+      };
+      posts.push(post);
+      return post;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
+});
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
